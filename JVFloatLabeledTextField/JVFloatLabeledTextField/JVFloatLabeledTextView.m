39920b66c35fa5247883db436cbbd6cdf4a5d296
Tim Lee <stequald@gmail.com>
2016-03-14 10:47:11 -0700

    always show floating label

--- JVFloatLabeledTextField/JVFloatLabeledTextField/JVFloatLabeledTextView.m
+++ JVFloatLabeledTextField/JVFloatLabeledTextField/JVFloatLabeledTextView.m
@@ -187,12 +187,7 @@ static CGFloat const kFloatingLabelHideAnimationDuration = 0.3f;
     BOOL firstResponder = self.isFirstResponder;
     _floatingLabel.textColor = (firstResponder && self.text && self.text.length > 0 ?
                                 self.labelActiveColor : self.floatingLabelTextColor);
-    if (!self.text || 0 == [self.text length]) {
-        [self hideFloatingLabel:firstResponder];
-    }
-    else {
-        [self showFloatingLabel:firstResponder];
-    }
+    [self showFloatingLabel:firstResponder];
 }
 
 - (UIColor *)labelActiveColor
